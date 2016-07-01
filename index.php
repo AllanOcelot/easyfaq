b543cb4499cec6cf191cbc6100cfdb7cc7e36b48
Allan McKernan <meagusxlr@hotmail.com>
2016-07-01 22:47:51 +0100

    Version One Release
    This is the first stable release of the plugin.
    File changes include:
    - Readme
    - Styling
    Added fontawesome icons to the title with open close states.
    - Added custom post type (FAQ), Added shortcode
    - Disabled the testing shortcode
    - Added the most basic version of accordian JS I could for open close
    functionality

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
  */
 
 
- //Hook into the WP head, and add our Styles
+ ##########################################################
+ ##### Add our Styles and Our Javascript to Wordpress #####
+ ##########################################################
  function easyFAQCSS() {
    //Insert our stylesheet
    //echo "<link rel='stylesheet'  href='/wp-content/plugins/easyfaq/assets/main.css' type='text/css' media='all'>";
@@ -27,7 +29,119 @@ function easyFAQJSFiles(){
 }
 add_action( 'wp_enqueue_scripts', 'easyFAQJSFiles' );
 
- //Spit out dummy HTML
+############################################################
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+###############################################
+##### Create Custom Post Type for our FAQ #####
+###############################################
+add_action( 'init', 'easyFAQPostType' );
+function easyFAQPostType() {
+	$labels = array(
+		'name'               => _x( 'FAQs', 'post type general name', 'your-plugin-textdomain' ),
+		'singular_name'      => _x( 'FAQ', 'post type singular name', 'your-plugin-textdomain' ),
+		'menu_name'          => _x( 'FAQs', 'admin menu', 'your-plugin-textdomain' ),
+		'name_admin_bar'     => _x( 'FAQ', 'add new on admin bar', 'your-plugin-textdomain' ),
+		'add_new'            => _x( 'Add New', 'FAQ', 'your-plugin-textdomain' ),
+		'add_new_item'       => __( 'Add New FAQ', 'your-plugin-textdomain' ),
+		'new_item'           => __( 'New FAQ', 'your-plugin-textdomain' ),
+		'edit_item'          => __( 'Edit FAQ', 'your-plugin-textdomain' ),
+		'view_item'          => __( 'View FAQ', 'your-plugin-textdomain' ),
+		'all_items'          => __( 'All FAQ', 'your-plugin-textdomain' ),
+		'search_items'       => __( 'Search FAQ', 'your-plugin-textdomain' ),
+		'not_found'          => __( 'No FAQs found.', 'your-plugin-textdomain' ),
+		'not_found_in_trash' => __( 'No FAQs found in Trash.', 'your-plugin-textdomain' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+    'description'        => __( 'Description.', 'your-plugin-textdomain' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'FAQs' ),
+		'capability_type'    => 'post',
+		'has_archive'        => false,
+		'hierarchical'       => false,
+    'menu_icon'          => 'dashicons-format-status',
+		'supports'           => array( 'title', 'editor', 'author')
+	);
+
+	register_post_type( 'FAQ', $args );
+}
+########################################################
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+########################################################
+##### Query the FAQ and put them into our UL ###########
+########################################################
+function easyFAQQuery(){
+
+  $args = array(
+  	'post_type' => 'FAQ',
+    'posts_per_page' => '-1'
+  );
+  $the_query = new WP_Query( $args );
+
+  // Loop Over Results
+  if ( $the_query->have_posts() ) {
+  	echo "<ul class='easy-faq-container'>";
+  	while ( $the_query->have_posts() ) {
+  		$the_query->the_post(); ?>
+      <li>
+        <div class="title"><?php echo the_title(); ?></div>
+        <div class="content">
+          <?php echo the_content(); ?>
+        </div>
+      </li>
+      <?php
+  	}
+  	echo '</ul>';
+  } else {
+  	// no posts found
+  }
+  /* Restore original Post Data */
+  wp_reset_postdata();
+}
+
+## Add funciton as shortcode so user can call it ##
+add_shortcode('easyFAQ' , 'easyFAQQuery');
+
+########################################################
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+########################################################
+##### Below simply for testing content #################
+########################################################
  function exampleFAQOutput(){
    ?>
     <ul class="easy-faq-container">
@@ -47,5 +161,4 @@ add_action( 'wp_enqueue_scripts', 'easyFAQJSFiles' );
   </ul>
   <?php
  }
-
  add_shortcode('easyFAQDummy' , 'exampleFAQOutput');
