6cb8ca26d45754ed309cdaf93081a4479275b177
trDEV2016 <tom@Toms-iMac.local>
2016-07-01 17:09:41 +0100

    JS Animation
    Began coding the animation of the title elements

--- assets/easyFAQ.js
+++ assets/easyFAQ.js
@@ -1,7 +1,17 @@
 jQuery(document).ready(function(){
   console.log("easyFAQ JS file has loaded!");
 
-  jQuery('.easy-faq-container .title').click(function(){
+  jQuery('.easy-faq-container li').click(function(){
+      if(jQuery(this).find('.title').hasClass('active')){
+          //do nothing
+      }else{
+          $('.easy-faq-container li .title.active').removeClass('active');
+          // remove the class active from any lli .title that have the class active, and slide their content out
+          // slide the content for $(this) down - so that the content is visible
+          // add the class active to this li's .title
+      }
+
+
 
   });
 });
