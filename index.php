f77f4631cb16bde51891883e0090f28f26983561
trDEV2016 <tom@Toms-iMac.local>
2016-07-01 13:43:22 +0100

    Reverted to original

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * Plugin Name: Wordpress EasyFAQs
  * Plugin URI:
  * Description: This plugin adds very quick to use FAQ functions to pages.
@@ -13,5 +13,4 @@
  add_action( 'wp_head', 'easyFAQ' );
 function easyFAQ() {
   echo "<link rel='stylesheet'  href='/wp-content/plugins/easyfaq/assets/main.css' type='text/css' media='all'>";
-  #Toms first Commit
 }
