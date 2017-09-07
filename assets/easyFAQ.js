651c2a5097b485fef1cce994455a64cb57adcd00
Allan Mckernan <meagusxlr@hotmail.com>
2017-09-07 18:28:08 +0100

    Improved the JS file a lot, god, a year ago I was so inefficient....

--- assets/easyFAQ.js
+++ assets/easyFAQ.js
@@ -1,22 +1,19 @@
   jQuery(document).ready(function(){
-   console.log("easyFAQ JS file has loaded!");
 
   jQuery('.easy-faq-container li .title').click(function(){
     var clickedLi = jQuery(this).parent('li');
-      if(clickedLi.find('.title').hasClass('active')){
-        //If the user clicks on the title again, it's probbably because they want to hide it
-        jQuery('.easy-faq-container li .title.active').parent('li').find('.content').slideUp(200, function(){
-         jQuery('.easy-faq-container li .title.active').removeClass('active');
-        }); 
-      }else{
-        //Find any FAQ that is open, hide it's content and remove the active class.
-         jQuery('.easy-faq-container li .title.active').parent('li').find('.content').slideUp(200, function(){
-          jQuery('.easy-faq-container li .title.active').removeClass('active');
-         });
-         //Get the current clicked li, find it's child element (content) slide it in so it's visible and add the class active to the li's child element ('title')
-        clickedLi.find('.content').slideDown(400, function(){
-          clickedLi.find('.title').addClass('active');
-        });
+    var active_titles = jQuery('.easy-faq-container li .title.active');
+
+    //Close any open ones when a user clicks on a new FAQ item or an open title
+    active_titles.parent('li').find('.content').slideUp(200, function(){
+     active_titles.removeClass('active');
+    });
+
+      if(clickedLi.find('.title').hasClass('active') == false){
+      //Get the current clicked li, find it's child element (content) slide it in so it's visible and add the class active to the li's child element ('title')
+       clickedLi.find('.content').slideDown(400, function(){
+         clickedLi.find('.title').addClass('active');
+       });
       }
    });
 
