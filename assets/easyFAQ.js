e0997cd8490a630b5d61fb14658bd3f34dce6f49
Allan Mckernan <meagusxlr@hotmail.com>
2017-09-07 18:11:03 +0100

    If a user clicks on an open item, it should close. If a user clicks inside an FAQ item, it will no longer close that FAQ.

--- assets/easyFAQ.js
+++ assets/easyFAQ.js
@@ -1,21 +1,24 @@
   jQuery(document).ready(function(){
    console.log("easyFAQ JS file has loaded!");
 
-  jQuery('.easy-faq-container li').click(function(){
-    var clickedLi = jQuery(this);
+  jQuery('.easy-faq-container li .title').click(function(){
+    var clickedLi = jQuery(this).parent('li');
       if(clickedLi.find('.title').hasClass('active')){
-          //do nothing
+        //If the user clicks on the title again, it's probbably because they want to hide it
+        jQuery('.easy-faq-container li .title.active').parent('li').find('.content').slideUp(200, function(){
+         jQuery('.easy-faq-container li .title.active').removeClass('active');
+        }); 
       }else{
         //Find any FAQ that is open, hide it's content and remove the active class.
          jQuery('.easy-faq-container li .title.active').parent('li').find('.content').slideUp(200, function(){
           jQuery('.easy-faq-container li .title.active').removeClass('active');
          });
          //Get the current clicked li, find it's child element (content) slide it in so it's visible and add the class active to the li's child element ('title')
-          clickedLi.find('.content').slideDown(400, function(){
-            clickedLi.find('.title').addClass('active');
-          });
+        clickedLi.find('.content').slideDown(400, function(){
+          clickedLi.find('.title').addClass('active');
+        });
       }
    });
 
-   
+
  });
